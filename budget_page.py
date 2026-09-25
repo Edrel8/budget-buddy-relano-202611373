25981ed2326f43ee74070d6bf5df41a1e357b869
Edrel8 <relanoedangelo@gmail.com>
2026-09-25 17:45:25 +0800

    display budget and expense data

--- budget_page.py
+++ budget_page.py
@@ -1,3 +1,10 @@
 import streamlit as st
+import data_loader as dl
 
 st.title('Budget Planner')
+
+def show_budget_data():
+    budget_data = dl.load_budget_data()
+    st.dataframe(budget_data)
+
+show_budget_data()
--- expense_page.py
+++ expense_page.py
@@ -1,3 +1,10 @@
 import streamlit as st
+import data_loader as dl
 
 st.title('Expenses')
+
+def show_expense_data():
+    expense_data = dl.load_expense_data()
+    st.dataframe(expense_data)
+
+show_expense_data()
